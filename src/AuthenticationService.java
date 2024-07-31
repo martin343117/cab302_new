37ad7150e1b06e08e9f2b36d98fff276651959bf
Theo <n11540869@qut.edu.au>
2024-07-31 17:41:12 +1000

    Create login method

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -4,12 +4,12 @@ import java.util.Objects;
 public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
 
-    // TODO Now: Add a constructor to initialize the users list with the default user
+    // Add a constructor to initialize the users list with the default user
     public AuthenticationService(){
         users = new ArrayList<User>();
     }
 
-    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    // Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
     @Override
     public User signUp(String username, String password) {
         for (User user : users){
@@ -25,6 +25,11 @@ public class AuthenticationService implements IAuthenticationService {
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     @Override
     public User logIn(String username, String password) {
+        for (User user : users){
+            if(user.getUsername().equals(username) & user.getPassword().equals(password)){
+                return user;
+            }
+        }
         return null;
     }
 
