1dd5415287fb0120162d5f255ee5c1ed531bed84
Theo <n11540869@qut.edu.au>
2024-07-31 17:38:54 +1000

    Create signup procedure

---
+++ src/AuthenticationService.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    // TODO Now: Add a constructor to initialize the users list with the default user
+    public AuthenticationService(){
+        users = new ArrayList<User>();
+    }
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    @Override
+    public User signUp(String username, String password) {
+        for (User user : users){
+            if(Objects.equals(user.getUsername(), username)){
+                return null;
+            }
+        }
+        User create_user = new User(username, password);
+        users.add(create_user);
+        return create_user;
+
+    }
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    @Override
+    public User logIn(String username, String password) {
+        return null;
+    }
+
+
+
+}
